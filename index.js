42621aca12fa123819d7c869f429ce183239f833
kamna <kamnabhadoriya042@gmail.com>
2024-02-16 11:10:29 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path'); 
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const UserSchema = require('./Schema/user');
 
 const app = express();
 
+mongoose.connect('mongodb://0.0.0.0:27017/Workshop')
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
+app.get('/register', async (req, res) => {
     try {
         res.sendFile(path.join(__dirname, 'index.html'));
     } catch (error) {
@@ -13,6 +20,23 @@ app.get('/', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+
+app.post('/register', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        console.log(req.body);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const User = mongoose.model('User', UserSchema); // Create model here
+        const user = new User({ username: name, email, password: hashedPassword });
+        await user.save();
+        res.status(201).send('User registered successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error registering user');
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
 
 app.listen(PORT, () => {
     console.log(`Application started at port ${PORT}`);
